f2f41776f91922a0387ba5289f2a58378932a885
reijjo <teemu.aitomeri@gmail.com>
2023-11-06 20:14:14 +0200

    deleted everything

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./assets/css/App.css";
-import "./assets/css/index.css";
-import "./assets/css/mobile-App.css";
-import "./assets/css/mobile-index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
